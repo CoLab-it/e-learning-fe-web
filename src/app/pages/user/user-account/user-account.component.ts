f6b2637d874f8ec556c9ff339d67dfe9a45b3fdf
Ahammed Shaheer <shaheerbinmuhammed6@gmail.com>
2024-04-20 11:22:29 +0530

    user can update user profile

--- src/app/pages/user/user-account/user-account.component.ts
+++ src/app/pages/user/user-account/user-account.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { HeaderComponent } from '../../../components/common/header/header.component';
 import { FooterComponent } from '../../../components/common/footer/footer.component';
 import { InputComponent } from '../../../components/common/input/input.component';
 import { ButtonComponent } from '../../../components/common/button/button.component';
 import { Router } from '@angular/router';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-user-account',
@@ -12,8 +13,8 @@ import { Router } from '@angular/router';
   templateUrl: './user-account.component.html',
   styleUrl: './user-account.component.css',
 })
-export class UserAccountComponent {
-  constructor(private router: Router) {}
+export class UserAccountComponent implements OnInit {
+  constructor(private router: Router, private userServ: UserService) {}
 
   @ViewChild('username') username: any;
   @ViewChild('email') email: any;
@@ -21,7 +22,25 @@ export class UserAccountComponent {
   @ViewChild('address') address: any;
 
   data: profile = {};
+  userProfile: any;
 
+  ngOnInit(): void {
+    this.userGetData();
+  }
+  userGetData() {
+    this.userServ.getUserProfile().subscribe({
+      next: (res) => {
+        this.userProfile = res.user[0];
+        this.username.value = this.userProfile?.name;
+        this.email.value = this.userProfile?.email;
+        this.number.value = this.userProfile?.phonenumber;
+        this.address.value = this.userProfile?.userProfile[0].address;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
   onLogout() {
     localStorage.clear();
     this.router.navigate(['/login']);
@@ -32,6 +51,14 @@ export class UserAccountComponent {
     this.data.number = this.number.value;
     this.data.address = this.address.value;
     console.log(this.data);
+    this.userServ.updateUserProfile(this.data).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
 
